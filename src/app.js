6e306a438b3b6bf729b0d452398804f451c9e274
Soudip Halder <soudip.halder@genesys.com>
2022-07-06 03:10:39 +0530

    1.0

---
+++ src/app.js
@@ -0,0 +1,205 @@
+const platformClient = require("purecloud-platform-client-v2");
+const {clientId, clientSecret} = require('./config')
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 81
+
+let userId = ''
+let skillChecked =''
+
+const client = platformClient.ApiClient.instance;
+client.setEnvironment(platformClient.PureCloudRegionHosts.us_east_1); 
+loginClient()
+function loginClient(){
+    client.loginClientCredentialsGrant(clientId,clientSecret)
+    .then((data)=> {
+        client.setAccessToken(data.accessToken)
+        app.get('/skillChecked', function(req, res){
+            userId = req.query.id
+            skillChecked = req.query.skill
+            console.log(userId)
+            console.log(skillChecked)
+            if(skillChecked=="true"){
+                let apiInstance = new platformClient.UsersApi();
+                let body = {
+                    "id": "cb8856d4-7647-43bf-9d32-c055af95dd70",
+                    "proficiency": 0
+                }; // Object | Skill
+
+                // Add routing skill to user
+                console.log("if block"+skillChecked+" "+userId)
+                apiInstance.postUserRoutingskills(userId, body)
+                .then((data1) => {
+                    console.log(data1)
+                })
+                .catch((err) => {
+                    refreshCode()
+                });
+            }
+            else{
+                let apiInstance = new platformClient.UsersApi();
+                let skillId = "cb8856d4-7647-43bf-9d32-c055af95dd70"; // String | skillId
+                console.log("else block"+skillChecked+" "+userId)
+                // Remove routing skill from user
+                apiInstance.deleteUserRoutingskill(userId, skillId)
+                .then((data1) => {
+                    console.log(data1)
+                })
+                .catch((err) => {
+                    refreshCode()
+                });
+            }
+        })
+    })
+    .catch((err) => {
+        refreshCode()
+    });
+} 
+
+
+function refreshCode() {
+    client.loginClientCredentialsGrant(clientId,clientSecret)
+    .then((data)=> {
+        client.setAccessToken(data.accessToken)
+            if(skillChecked=="true"){
+                let apiInstance = new platformClient.UsersApi();
+                let body = {
+                    "id": "cb8856d4-7647-43bf-9d32-c055af95dd70",
+                    "proficiency": 0
+                }; // Object | Skill
+
+                // Add routing skill to user
+                console.log("if block"+skillChecked+" "+userId)
+                apiInstance.postUserRoutingskills(userId, body)
+                .then((data1) => {
+                    console.log(data1)
+                })
+                .catch((err) => {
+                    console.log(err)
+                });
+            }
+            else{
+                let apiInstance = new platformClient.UsersApi();
+                let skillId = "cb8856d4-7647-43bf-9d32-c055af95dd70"; // String | skillId
+                console.log("else block"+skillChecked+" "+userId)
+                // Remove routing skill from user
+                apiInstance.deleteUserRoutingskill(userId, skillId)
+                .then((data1) => {
+                    console.log(data1)
+                })
+                .catch((err) => {
+                    console.log(err)
+                });
+            }
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
+    }
+
+// function skillOperation(){
+//     console.log("skillOperation")
+// }
+// client.loginClientCredentialsGrant(clientId,clientSecret)
+// .then((data)=> {
+//     client.setAccessToken(data.accessToken)
+//     app.get('/skillChecked', function(req, res){
+//         const userId = req.query.id
+//         const skillChecked = req.query.skill
+//         console.log(userId)
+//         console.log(skillChecked)
+//         if(skillChecked=="true"){
+//             let apiInstance = new platformClient.UsersApi();
+//             let body = {
+//                 "id": "cb8856d4-7647-43bf-9d32-c055af95dd70",
+//                 "proficiency": 0
+//             }; // Object | Skill
+
+//             // Add routing skill to user
+//             console.log("if block"+skillChecked+" "+userId)
+//             apiInstance.postUserRoutingskills(userId, body)
+//             .then((data1) => {
+//                 res.send({
+//                     output:"Success"
+//                 })
+//                 console.log(data1)
+//             })
+//             .catch((err) => {
+//                 res.send({
+//                     output:"Failure"
+//                 })
+//                 console.log(err)
+//             });
+//         }
+//         else{
+//             let apiInstance = new platformClient.UsersApi();
+//             let skillId = "cb8856d4-7647-43bf-9d32-c055af95dd70"; // String | skillId
+//             console.log("else block"+skillChecked+" "+userId)
+//             // Remove routing skill from user
+//             apiInstance.deleteUserRoutingskill(userId, skillId)
+//             .then((data1) => {
+//                 res.send({
+//                     output:"Success"
+//                 })
+//                 console.log(data1)
+//             })
+//             .catch((err) => {
+//                 res.send({
+//                     output:"Failure"
+//                 })
+//                 console.log(err)
+//             });
+//         }
+//   })
+// })
+// .catch((err) => {
+//  console.log(err);
+// });
+
+// app.get('/transferAnotherQueue', function(req, res){
+//     const interactionID = req.query.interactionID
+//     const queueName = req.query.queueName
+//     getParticipantID(interactionID, platformClient, function(error, data){
+//         try{
+//             let participantID = data.participantID
+//             getQueueID(queueName, platformClient, function(error, data){
+//                 try{
+//                     const queueID = data.queueID
+//                     let body = {
+//                         "userId": "",
+//                         "address": "",
+//                         "userName": "",
+//                         "queueId": queueID,
+//                         "voicemail": false
+//                     };
+//                     postReplaceInteraction(interactionID, participantID, body, platformClient, function(error, data){
+//                         try{
+//                             res.send({
+//                                 "output": data.output
+//                             })
+//                         }catch(e){
+//                             res.send({
+//                                 error:error
+//                             })
+//                         }
+//                     })
+//                 }catch(e){
+//                     res.send({
+//                         error : error
+//                     })
+//                 }
+//             })
+            
+//         }
+//         catch(e){
+//             res.send({
+//                 error : error
+//             })
+//         }
+//     })
+// })
+
+app.listen(port,function(){
+    console.log('server is running on port '+port)
+})
---
+++ src/config.js
@@ -0,0 +1,6 @@
+const config = {
+    clientId: 'eee840f2-3062-4b53-b1df-99869fd30ba8',
+    clientSecret: 'm_Z_BqhIYHCUhJ4vldsiAgbUZXVJz91ejLXLPB3K_Qk',
+}
+
+module.exports = config
